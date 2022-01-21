eb8d7dec8af774d4aa1ffd39720a22285d875a49
Hikari <hikarihashida7@gmail.com>
2022-01-21 20:26:39 +1100

    removed executables

--- code/quad/controllers/simple-cpp/build/release/simple-cpp.d
+++
@@ -1,5 +0,0 @@
-build/release/simple-cpp.o: simple-cpp.cpp \
-  /Applications/Webots.app/include/controller/cpp/webots/Motor.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/Device.hpp \
-  /Applications/Webots.app/include/controller/cpp/webots/../../c/webots/types.h \
-  /Applications/Webots.app/include/controller/cpp/webots/Robot.hpp
